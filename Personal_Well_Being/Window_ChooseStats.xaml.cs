3c23a4a71bc398d167ce288d3b462fecac7b5241
Rick Roslof <rroslof@eecs.wsu.edu>
2023-04-08 02:12:03 -0700

    Fixed dialog window templates.

--- Personal_Well_Being/Window_ChooseStats.xaml.cs
+++ Personal_Well_Being/Window_ChooseStats.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Personal_Well_Being
 {
@@ -26,7 +14,13 @@ namespace Personal_Well_Being
 
         private void AcceptButton_Click(object sender, RoutedEventArgs e)
         {
+            DialogResult = true;
+            this.Close();
+        }
 
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- Personal_Well_Being/Window_DefineSkills.xaml.cs
+++ Personal_Well_Being/Window_DefineSkills.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Personal_Well_Being
 {
@@ -22,6 +10,17 @@ namespace Personal_Well_Being
         public Window_DefineSkills()
         {
             InitializeComponent();
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
